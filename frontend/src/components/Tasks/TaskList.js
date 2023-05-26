53beed1436db0d08fc2e25f7edd089bd0ecfd836
MUKUND <mukund@2304H-MUKUND.local>
2023-05-26 16:18:14 +0800

    added task filter functionality

--- frontend/src/components/Tasks/TaskList.js
+++ frontend/src/components/Tasks/TaskList.js
@@ -1,23 +1,35 @@
+import { useState, Fragment } from "react";
 import { useSelector } from "react-redux";
 import TaskItem from "./TaskItem";
 import classes from "./TaskList.module.css";
 
 function TaskList() {
-	const tasks = useSelector((state) => state.task.tasks);
+	const [filterTitle, setFilterTitle] = useState("");
 
-	return (
-		<div className={classes["task-list"]}>
-			{tasks.map((task) => (
-				<TaskItem
-					key={task.id}
-					id={task.id}
-					title={task.title}
-					desc={task.desc}
-					deadline={task.deadline}
-				/>
-			))}
-		</div>
-	);
+  const tasks = useSelector((state) => state.task.tasks);
+
+  const filterHandler = (event) => {
+	setFilterTitle(event.target.value);
+  };
+
+  const filteredTasks = tasks.filter((task) => task.title.includes(filterTitle));
+
+  return (
+    <Fragment>
+      <input className={classes["task-finder"]} onChange={filterHandler}/>
+      <div className={classes["task-list"]}>
+        {filteredTasks.map((task) => (
+          <TaskItem
+            key={task.id}
+            id={task.id}
+            title={task.title}
+            desc={task.desc}
+            deadline={task.deadline}
+          />
+        ))}
+      </div>
+    </Fragment>
+  );
 }
 
 export default TaskList;
